2b0fbb95858e51df161e78f591bdad7208ebb647
progmamun <mamun333089@gmail.com>
2022-03-22 10:25:45 +0600

    components-layout

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './Shop.css';
+
+const Shop = () => {
+  const [] = useState([]);
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        <h3>This is products</h3>
+      </div>
+      <div className="cart-container">
+        <h3>Order Summery</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
