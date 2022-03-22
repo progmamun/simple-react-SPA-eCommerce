0dafeae6dfc6f85c0f875133d5be9d6745613c78
progmamun <mamun333089@gmail.com>
2022-03-22 23:55:45 +0600

    total-shipping

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,16 +3,19 @@ import './Cart.css';
 
 const Cart = props => {
   const { cart } = props;
+  // console.log(cart);
   let total = 0;
+  let shipping = 0;
   for (const proudct of cart) {
     total = total + proudct.price;
+    shipping = shipping + proudct.shipping;
   }
   return (
     <div className="cart">
       <h3>Order Summery</h3>
       <p>Selected Items: {cart.length}</p>
       <p>Total Price: ${total}</p>
-      <p>Total Shipping Charge</p>
+      <p>Total Shipping Charge: ${shipping}</p>
       <p>Tax</p>
       <h3>Grand Total: </h3>
     </div>
