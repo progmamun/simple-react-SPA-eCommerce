67fce86481be2d044e1deebcd7ba78c907977876
progmamun <mamun333089@gmail.com>
2022-03-22 12:31:23 +0600

    product-cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,7 +7,16 @@ const Product = props => {
   return (
     <div className="product">
       <img src={img} alt="" />
-      <p className="product-name">{name}</p>
+      <div className="product-info">
+        <p className="product-name">{name}</p>
+        <p>Price: {price}</p>
+        <p>
+          <small>Ratings: {ratings} stars</small>
+        </p>
+        <p>
+          <small>Seller: {seller}</small>
+        </p>
+      </div>
     </div>
   );
 };
