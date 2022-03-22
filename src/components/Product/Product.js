3a66c005bc0b6aecab5652ddb2409daa3ce39398
progmamun <mamun333089@gmail.com>
2022-03-22 13:45:29 +0600

    set-eventHandler

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,6 +4,7 @@ import './Product.css';
 const Product = props => {
   console.log(props);
   const { name, img, seller, price, ratings } = props.product;
+
   return (
     <div className="product">
       <img src={img} alt="" />
@@ -17,7 +18,12 @@ const Product = props => {
           <small>Seller: {seller}</small>
         </p>
       </div>
-      <button className="btn-cart">Add to Cart</button>
+      <button
+        onClick={() => props.handleAddToCart(props.product)}
+        className="btn-cart"
+      >
+        Add to Cart
+      </button>
     </div>
   );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -10,11 +10,20 @@ const Shop = () => {
       .then(res => res.json())
       .then(data => setProducts(data));
   }, []);
+
+  const handleAddToCart = product => {
+    console.log(product);
+  };
+
   return (
     <div className="shop-container">
       <div className="products-container">
         {products.map(product => (
-          <Product key={product.id} product={product}></Product>
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
         ))}
       </div>
       <div className="cart-container">
