a4f5c6c300c56f2f1dd79d31f2f2bd9dfd57acdd
progmamun <mamun333089@gmail.com>
2022-03-22 23:34:33 +0600

    shopping-cart-price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({ cart }) => {
+const Cart = props => {
+  const { cart } = props;
+  let total = 0;
+  for (const proudct of cart) {
+    total = total + proudct.price;
+  }
   return (
-    <div>
+    <div className="cart">
       <h3>Order Summery</h3>
       <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping Charge</p>
+      <p>Tax</p>
+      <h3>Grand Total: </h3>
     </div>
   );
 };
