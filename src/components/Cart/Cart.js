d65fbbdb0bb9616085403db041199b8412945e18
progmamun <mamun333089@gmail.com>
2022-03-23 00:00:22 +0600

    tax-toFixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,13 +10,15 @@ const Cart = props => {
     total = total + proudct.price;
     shipping = shipping + proudct.shipping;
   }
+  const tax = total * 0.1;
+
   return (
     <div className="cart">
       <h3>Order Summery</h3>
       <p>Selected Items: {cart.length}</p>
       <p>Total Price: ${total}</p>
       <p>Total Shipping Charge: ${shipping}</p>
-      <p>Tax</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
       <h3>Grand Total: </h3>
     </div>
   );
