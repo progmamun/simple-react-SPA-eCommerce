0633875379557a15f16c745f97c4e89ad1c4b502
progmamun <mamun333089@gmail.com>
2022-03-22 10:30:27 +0600

    load-data-set-state

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
 
 const Shop = () => {
-  const [] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('products.json')
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
   return (
     <div className="shop-container">
       <div className="products-container">
-        <h3>This is products</h3>
+        <h3>This is products: {products.length}</h3>
       </div>
       <div className="cart-container">
         <h3>Order Summery</h3>
