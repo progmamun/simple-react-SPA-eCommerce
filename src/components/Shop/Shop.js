8714bc6d5ef57b8adcc982508a63ff89bbd83b3d
progmamun <mamun333089@gmail.com>
2022-03-23 19:41:39 +0600

    cart-with-quantity

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -31,12 +31,21 @@ const Shop = () => {
     setCart(savedCart);
   }, [products]);
 
-  const handleAddToCart = product => {
-    // console.log(product);
-    // cart.push(product);
-    const newCart = [...cart, product];
+  const handleAddToCart = selectedProduct => {
+    // console.log(selectedProduct);
+    // cart.push(selectedProduct);
+    let newCart = [];
+    const exists = cart.find(product => product.id === selectedProduct.id);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter(product => product.id !== selectedProduct.id);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
     setCart(newCart);
-    addToDb(product.id);
+    addToDb(selectedProduct.id);
   };
 
   return (
