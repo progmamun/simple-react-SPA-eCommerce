d30a20fb2cd1ea2c888360738220a473ce9b743a
progmamun <mamun333089@gmail.com>
2022-03-31 20:08:50 +0600

    review-item-style

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import './ReviewItem.css';
 
 const ReviewItem = props => {
-  const { name, price, shipping, quantity } = props.product;
+  const { name, img, price, shipping, quantity } = props.product;
   return (
-    <div>
-      <h2>review item: {name}</h2>
+    <div className="review-item">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div className="review-item-details-container">
+        <div className="review-item-details">
+          <p className="product-name" title={name}>
+            {name.length > 20 ? name.slice(0, 20) + '...' : name}
+          </p>
+          <p>
+            <span className="orange-color">{price}</span>
+          </p>
+          <p>
+            <small>shipping: {shipping}</small>
+          </p>
+          <p>
+            <small>Quantity: {quantity}</small>
+          </p>
+        </div>
+        <div className="delete-container">
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 };
