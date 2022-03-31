c4e85e3d456db4eceb7ffc5331e88a7c10e3e3f2
progmamun <mamun333089@gmail.com>
2022-03-31 17:45:47 +0600

    useCart-products-dependency

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart();
   return (
     <div>
       <h2>This is Orders</h2>
