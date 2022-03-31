442ff6257532e25bb99a5c7d3a0b5efa1a9287e2
progmamun <mamun333089@gmail.com>
2022-04-01 01:27:01 +0600

    data-consistent-UI-children-props

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import { removeFromDb } from '../../utilities/fakedb';
@@ -27,7 +28,11 @@ const Orders = () => {
           ))}
         </div>
         <div className="cart-container">
-          <Cart cart={cart}></Cart>
+          <Cart cart={cart}>
+            <Link>
+              <button>Proceed Checkout</button>
+            </Link>
+          </Cart>
         </div>
       </div>
     </div>
