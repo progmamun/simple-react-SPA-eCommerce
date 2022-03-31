05351eaf1ab95a5556174f48be36adc5c4d83c97
progmamun <mamun333089@gmail.com>
2022-03-31 17:04:28 +0600

    useState-useEffect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
 import Orders from './components/Orders/Orders';
 import Inventory from './components/Inventory/Inventory';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/shop" element={<Shop></Shop>}></Route>
         <Route path="/orders" element={<Orders></Orders>}></Route>
         <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
       </Routes>
     </div>
   );
