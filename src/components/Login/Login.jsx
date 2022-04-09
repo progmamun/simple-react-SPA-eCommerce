eb0607d48330948da18a9e5c1c9e3472081bbae8
progmamun <mamun333089@gmail.com>
2022-04-10 05:08:46 +0600

    login-component-style

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
   return (
-    <div>
-      <h2>this is login components</h2>
+    <div className="form-container">
+      <div>
+        <h2 className="form-title">Login</h2>
+        <form action="">
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="" />
+          </div>
+          <input className="form-submit" type="submit" value="login" />
+        </form>
+        <p>
+          New to Ema-John?{' '}
+          <Link className="form-link" to="/signup">
+            Create an account
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
