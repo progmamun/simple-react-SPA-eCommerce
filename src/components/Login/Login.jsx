58da896d82b224b52533fa4e64b843c819de0c1a
progmamun <mamun333089@gmail.com>
2022-04-09 23:50:04 +0600

    updatte

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>this is login components</h2>
+    </div>
+  );
+};
+
+export default Login;
