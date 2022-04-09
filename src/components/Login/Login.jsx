7506926ddfc4c343cf4770a6219bbb555b857790
progmamun <mamun333089@gmail.com>
2022-04-10 05:39:38 +0600

    style-sign-up

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -10,11 +10,11 @@ const Login = () => {
         <form action="">
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="" />
+            <input type="email" name="email" id="" required />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="" />
+            <input type="password" name="password" id="" required />
           </div>
           <input className="form-submit" type="submit" value="login" />
         </form>
@@ -24,6 +24,11 @@ const Login = () => {
             Create an account
           </Link>
         </p>
+        <div className="form-hr">
+          <hr />
+          <p>or</p>
+          <hr />
+        </div>
       </div>
     </div>
   );
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css';
 
 const SignUp = () => {
   return (
-    <div>
-      <h3>this is sign up</h3>
+    <div className="form-container">
+      <div>
+        <h2 className="form-title">Sign Up</h2>
+        <form action="">
+          <div className="input-group">
+            <label htmlFor="name">Your Name</label>
+            <input type="text" name="name" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input type="password" name="confirm-password" id="" required />
+          </div>
+          <input className="form-submit" type="submit" value="sign Up" />
+        </form>
+        <p>
+          Already have an account?{' '}
+          <Link className="form-link" to="/login">
+            Login
+          </Link>
+        </p>
+        <div className="form-hr">
+          <hr />
+          <p>or</p>
+          <hr />
+        </div>
+      </div>
     </div>
   );
 };
