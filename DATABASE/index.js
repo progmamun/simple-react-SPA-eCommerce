d0523fcc09e35f6afc582bb75b1eb84cc785bc95
progmamun <mamun333089@gmail.com>
2022-04-24 00:19:15 +0600

    connect-mongodb

--- DATABASE/index.js
+++ DATABASE/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -6,6 +7,17 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pgqp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('John is running');
